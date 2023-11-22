8361a29d0df99bcc37c4708dc971f7fcee1a0fc6
Andrea Terenziani <andrea.terenziani.at@gmail.com>
2023-11-22 21:54:34 +0000

    Painful PySMT shite

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # MultiCouriers
 Project made for the UniBo course in Combinatorial Decision Making &amp; Optimization (2022/2023)
+
+## Before running
+
+First of all, make sure you install pipenv and that you install all dependencies with it
+
+```shell
+pip install pipenv
+# (cd into the repo's directory)
+pipenv install
+pipenv shell # activate virtual environment
+```
+
+Then, use the newly installed `pysmt` package to install the z3 and MSAT solvers
+
+```shell
+pysmt-install --z3
+pysmt-install --msat
+
+pysmt-install --check # to make sure they both installed correcly 
+```
--- SMT/mainSMT.py
+++ SMT/mainSMT.py
@@ -1,20 +1,10 @@
-"""from pysmt.shortcuts import *
-
-tie = Symbol("tie")
-shirt = Symbol("shirt")
-f1 = Or(tie, shirt)
-f2 = Or(Not(tie), shirt)
-f3 = Or(Not(tie), Not(shirt))
-f = And(f1, f2, f3)
-
-print(f)
-print(f"Satifiable: {is_sat(f)}")
-print(f"Model:\n{get_model(f)}")
-print(get_model(f).get_value(tie))"""
 from multiprocessing import freeze_support
-from pysmt.shortcuts import Symbol, Implies, TRUE, FALSE, Not, is_sat, Portfolio
+from pysmt.shortcuts import *
 from pysmt.logics import QF_UFLIRA
 import logging
+
+def values_for_vars(solver, vars):
+    return {var: solver.get_value(var) for var in vars}
 
 # Portfolio solving makes it possible to run multiple solvers in
 # parallel.  As soon as the first solver completes, all other solvers
@@ -24,24 +14,31 @@ import logging
 # the solver state.
 #
 def main():
-    x, y = Symbol("x"), Symbol("y")
-    f = Implies(x, y)
-
-    # The first example shows how to use multiple solvers in the with the
-    # is_sat shortcut
-
     # We enable logging to see what is going on behind the scenes:
     _info = logging.getLogger(__name__).info
     logging.basicConfig(level=logging.INFO)
 
+    solvers_set = ["z3"]#, "msat"]
+
+    # The first example shows how to use multiple solvers in the with the
+    # is_sat shortcut
+
     # A solver set is an iterable of solver names or pairs of
     # solvers+options (See next example)
 
-    _info("Example 1: is_sat")
-    solvers_set = ["z3"]
+    tie = Symbol("tie")
+    shirt = Symbol("shirt")
+    f1 = Or(tie, shirt)
+    f2 = Or(Not(tie), shirt)
+    f3 = Or(Not(tie), Not(shirt))
+    f = And(f1, f2, f3)
+
     res = is_sat(f, portfolio=solvers_set)
-    _info(f"Example 1 res: {res}")
-    assert res is True
+    _info("Example 1: is_sat")
+    _info(f)
+    _info(f"Satifiable: {res}")
+    _info(f"Model:\n{get_model(f)}")
+    _info(f"Value for '{tie}': {get_model(f).get_value(tie)}")
 
     # Behind the scenes, pySMT launched 3 processes and solved the
     # expression in parallel.
@@ -55,42 +52,18 @@ def main():
     # particular, we are enabling incrementality and model generation.
 
     _info("Example 2: Portfolio()")
+
     with Portfolio(solvers_set,
                    logic=QF_UFLIRA,
                    incremental=True,
                    generate_models=True) as s:
-        print(s.__dict__)
         s.add_assertion(f)
-        s.push()
-        s.add_assertion(x)
+
         res = s.solve()
-        _info(f"Example 2 res1: {res}")
-        v_y = s.get_value(y)
-        assert v_y is TRUE()
-
-        s.pop()
-        s.add_assertion(Not(y))
-        res = s.solve()
-        _info(f"Example 2 res2: {res}")
-        v_x = s.get_value(x)
-        assert v_x is FALSE()
-
-
-    # Portfolio can also be useful to tweak heuristics of the solvers.
-    # For supported solver options, please refer to the solver
-    # documentation. This is an area of pySMT that could use additional
-    # feedback and help!
-    """
-    NOT WORKING CAUSE WHO KNOWS WHAT OPTIONS z3 HAS
-    
-    _info("Example 3: Portfolio w options")
-    with Portfolio(solvers_set=["z3"],
-                   logic=QF_UFLIRA,
-                   incremental=False,
-                   generate_models=False) as s:
-        res = s.is_sat(f)
-        _info(msg=f"Example 3 res: {res}")
-        assert res is True"""
+        _info(f"Satifiable: {res}")
+        # _info(f"Model:\n{s.get_model()}") # This is not working like get_model() for some reason
+        for var in [tie, shirt]:
+            _info(f"Value for '{var}': {s.get_value(var)}")
 
 if __name__ == '__main__':
     freeze_support()
